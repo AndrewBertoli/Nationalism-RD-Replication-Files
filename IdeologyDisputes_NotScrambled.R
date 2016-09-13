07b5b84341cfecf19986ce299029d6394a62b325
Andrew Bertoli <abertoli@berkeley.edu>
2016-09-12 21:25:20 -0700

    Updated Replicaiton Code

---
+++ IdeologyDisputes_NotScrambled.R
@@ -0,0 +1,35 @@
+USA 1877             		1543 (L,I,R), 1637 (L), and 1681 (L,I,R)
+Finland 1956         		None
+Costa Rica 1966      		None
+France 1974  				361 (H), 2150 (H), 3012 (H,I,R), 3631 (H), 3016 (L,I), 3633 (L,I)
+Dominican Republic 1986   	2736 (L,I)
+Cyprus 1993   				4039 (H,I), 4095 (H,R)
+Dominican Republic 1994   	None 
+Dominican Republic 1996   	None
+Israel 1996   			    4273 (L,I)
+Madagascar 1996   			None
+Colombia 1998    			4263 (H,I)
+Costa Rica 1998   			4147 (L,R)
+Cyprus 1998   				None
+USA 2001   					4280 (L), 4281 (L), 4283 (H,I,R), 4336 (L), 4450 (L), 4451 (H,I), 4552 (H,R),
+							4455 (L,I,R), 4460 (L,I), 4512 (H,I), 4516 (H,I), 4371 (L), 4465 (L,I,R), 
+							4518 (H,I,R), 4519 (H), 4524 (L,I,R)
+Mexico 2006   				None
+Mongolia 2009   			None
+
+
+
+
+Austria 1957  				4877 (L,I), 4878 (L,I)
+Austria 1965   				None
+Austria 1974   				None
+Colombia 1978  				3120 (L,I), 2323 (H,R)
+Portugal 1986   			None
+Cyprus 1988   				2761 (H), 4038 (H)
+Colombia 1994   			None
+Costa Rica 1994   			4146 (H,I)
+South Korea 1997   			4125, (L,I,R), 4284 (H,R)
+Taiwan 2000   				4281 (L), 4444 (L,I,R), 4461 (L,I,R), 4462 (L,I,R)
+South Korea 2002   			4468 (L), 4473 (H), 4476 (L,I)
+Taiwan 2004   				4469 (L,I,R), 4470 (L,I,R)
+Finland 2006   				None
